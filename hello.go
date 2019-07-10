d4483e216662a52fa37955db2465f18acbd0896b
Cosmin Potocean <cosmycx@gmail.com>
2019-07-10 10:41:04 -0400

    added test web

--- hello.go
+++ hello.go
@@ -1,11 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println(Hello())
+
+	http.HandleFunc("/", home)
+
+	http.ListenAndServe(":8080", nil)
 } // .main
 
 func Hello() string {
 	return "Hello, world."
-}
+} // .Hello
+
+func home(w http.ResponseWriter, r *http.Request) {
+
+	w.Write([]byte(Hello()))
+} // .home
